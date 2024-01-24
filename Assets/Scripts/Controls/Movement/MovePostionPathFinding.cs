aa9312dc5708b2ed85801773ca165b86fc77e4ea
timpt0261 <rchavez0215@gmail.com>
2024-01-23 19:58:01 -0800

    Merge branch 'character-controller' of https://github.com/gold-mari/RoguelikeDeckbuilder into character-controller

---
+++ Assets/Scripts/Controls/Movement/MovePostionPathFinding.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class MovePostionPathFinding : MonoBehaviour
+{
+    public void SetMovePostion(Vector3 movePostion ){
+        
+
+    }
+}
