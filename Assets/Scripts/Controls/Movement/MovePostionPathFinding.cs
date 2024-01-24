3afe67ba341b6b30008450882f4f8e55af6bb3dd
timpt0261 <rchavez0215@gmail.com>
2024-01-23 21:16:25 -0800

    Added Rudimentary Character Controller
    Includes :
    - character controls with wasd , mouse, and pathFinding

--- Assets/Scripts/Controls/Movement/MovePostionPathFinding.cs
+++ Assets/Scripts/Controls/Movement/MovePostionPathFinding.cs
@@ -1,10 +1,42 @@
 using UnityEngine;
-using UnityEngine.Rendering;
+using UnityEngine.AI;
 
 public class MovePostionPathFinding : MonoBehaviour
 {
-    public void SetMovePostion(Vector3 movePostion ){
-        
+    [SerializeField] private NavMeshAgent _agent;
+
+    private void SetDestination(Vector3 destination)
+    {
+        _agent.destination = destination;
 
     }
+
+    private static Vector3 GetMouseWorldPostion()
+    {
+        Vector3 screenPostion = Input.mousePosition;
+        Vector3 worldPostion = Camera.main.ScreenToWorldPoint(screenPostion);
+        worldPostion.z = 0;
+
+        return worldPostion;
+    }
+
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.updateRotation = false;
+        _agent.updateUpAxis = false;
+    }
+
+    private void Update()
+    {
+
+        Vector3 mousePos = GetMouseWorldPostion();
+        Debug.Log(mousePos);
+        if (Input.GetMouseButton(0))
+        {
+            SetDestination(mousePos);
+
+        }
+    }
+
 }
