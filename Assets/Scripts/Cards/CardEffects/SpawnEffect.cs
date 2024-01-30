02e027dba97255cb6e594e3b8c45bf79a0f8205c
Dylan Mahler <dmahler@ucsc.edu>
2024-01-29 17:17:25 -0800

    Added random offset parameter to SpawnEffect

--- Assets/Scripts/Cards/CardEffects/SpawnEffect.cs
+++ Assets/Scripts/Cards/CardEffects/SpawnEffect.cs
@@ -1,25 +1,33 @@
 using UnityEngine;
 
 [System.Serializable]
-//[CreateAssetMenu(fileName = "New SpawnEffect", menuName = "Cards/CardEffect/SpawnEffect")]
 public class SpawnEffect : CardEffect
 {
     public GameObject toSpawn;
+    [Tooltip("The (min, max) horizontal range of randomness, applied as an offset from the target position.")]
+    public Vector2 xVariance = new();
+    [Tooltip("The (min, max) vertical range of randomness, applied as an offset from the target position.")]
+    public Vector2 yVariance = new();
     public SpawnEffect() { Debug_ID = "spawn"; }
 
 
-    
+
     public override void Activate(CardUser caller, Card card, Targetable target)
     {
-        GameObject.Instantiate(toSpawn, target.transform.position, Quaternion.identity);
+        GameObject.Instantiate(toSpawn, target.transform.position+GetOffset(), Quaternion.identity);
 
         EndEffect(card);
     }
 
     public override void Activate(CardUser caller, Card card, Vector3 target)
     {
-        GameObject.Instantiate(toSpawn, target, Quaternion.identity);
+        GameObject.Instantiate(toSpawn, target+GetOffset(), Quaternion.identity);
 
         EndEffect(card);
+    }
+
+    private Vector3 GetOffset()
+    {
+        return new(Random.Range(xVariance.x, xVariance.y), Random.Range(yVariance.x, yVariance.y), 0);
     }
 }
