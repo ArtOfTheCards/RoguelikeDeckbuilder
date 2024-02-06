f82fad8984032e80d4677fc8f1888c6d61128bf9
timpt0261 <rchavez0215@gmail.com>
2024-02-06 15:42:42 -0800

    Created Animations

--- Assets/Scripts/Controls/InputHandling/PlayerMovementKeys.cs
+++ Assets/Scripts/Controls/InputHandling/PlayerMovementKeys.cs
@@ -1,22 +1,45 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class PlayerMovementKeys : MonoBehaviour
 {
+    private Animator animator;
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     private void Update()
     {
-        float moveX = 0f;
-        float moveY = 0f;
+        // Get the movement input
+        Vector2 input = GetInput();
 
-        if (Input.GetKey(KeyCode.W)) moveY = +1f;
-        if (Input.GetKey(KeyCode.S)) moveY = -1f;
-        if (Input.GetKey(KeyCode.A)) moveX = -1f;
-        if (Input.GetKey(KeyCode.D)) moveX = +1f;
+        // Set animator parameters
+        animator.SetFloat("X", input.x);
+        animator.SetFloat("Y", input.y);
 
-        Vector3 moveVector = new Vector3(moveX, moveY).normalized;
-        GetComponent<IMoveVelocity>().SetVelocity(moveVector);
+        // Set walking parameter based on input
+        animator.SetBool("isWalking", input != Vector2.zero);
 
+        // Set velocity
+        GetComponent<IMoveVelocity>().SetVelocity(input);
+    }
 
+    private Vector2 GetInput()
+    {
+        Vector2 input = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+            input.y = 1f;
+        else if (Input.GetKey(KeyCode.S))
+            input.y = -1f;
+
+        if (Input.GetKey(KeyCode.A))
+            input.x = -1f;
+        else if (Input.GetKey(KeyCode.D))
+            input.x = 1f;
+
+        return input.normalized;
     }
 }
