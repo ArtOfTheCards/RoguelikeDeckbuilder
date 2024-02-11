1d6ee2fe5def734b8843024cacb5da2ac3ecb5f6
Reuben Chavez <rchavez0215@gmail.com>
2024-02-10 20:15:26 -0800

    Rebind controller (#7)
    * Options && New Player
    
    - Created an options screen w/ ability to switch between controls
    - Created new player prefab that is more organized and has more modular controls
    
    * Finished Rebind System

--- Assets/Scripts/Controls/Movement/PlayerMovement/MovePostionPathFinding.cs
+++ Assets/Scripts/Controls/Movement/PlayerMovement/MovePostionPathFinding.cs
@@ -1,42 +1,52 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public class MovePostionPathFinding : MonoBehaviour
+public class MovePositionPathfinding : MonoBehaviour
 {
-    [SerializeField] private NavMeshAgent _agent;
-
-    private void SetDestination(Vector3 destination)
-    {
-        _agent.destination = destination;
-
-    }
-
-    private static Vector3 GetMouseWorldPostion()
-    {
-        Vector3 screenPostion = Input.mousePosition;
-        Vector3 worldPostion = Camera.main.ScreenToWorldPoint(screenPostion);
-        worldPostion.z = 0;
-
-        return worldPostion;
-    }
-
-    private void Awake()
-    {
-        _agent = GetComponent<NavMeshAgent>();
-        _agent.updateRotation = false;
-        _agent.updateUpAxis = false;
-    }
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private Animator animator;
 
     private void Update()
     {
-
-        Vector3 mousePos = GetMouseWorldPostion();
-        // Debug.Log(mousePos);
         if (Input.GetMouseButton(0))
-        {
-            SetDestination(mousePos);
-
-        }
+            SetDestination(GetMouseWorldPosition());
     }
 
+    private void SetDestination(Vector3 destination)
+    {
+        agent.destination = destination;
+        PlayMovementAnimation(destination - transform.position);
+    }
+
+    private void PlayMovementAnimation(Vector3 movementDirection)
+    {
+        float angle = Vector3.SignedAngle(Vector3.forward, movementDirection.normalized, Vector3.up);
+        UpdateAnimatorParameters(angle);
+    }
+
+    private void UpdateAnimatorParameters(float angle)
+    {
+        float x = 0f, y = 0f;
+
+        if (angle >= -45 && angle < 45)
+            y = 1f; // Forward
+        else if (angle >= 45 && angle < 135)
+            x = 1f; // Right
+        else if (angle >= -135 && angle < -45)
+            x = -1f; // Left
+        else
+            y = -1f; // Backward
+
+        animator.SetFloat("X", x);
+        animator.SetFloat("Y", y);
+        animator.SetBool("isWalking", Mathf.Abs(x) > 0.1f || Mathf.Abs(y) > 0.1f);
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 screenPosition = Input.mousePosition;
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+        worldPosition.z = 0;
+        return worldPosition;
+    }
 }
