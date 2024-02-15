921e22e326ff0e03358bd41f6c85caded1be886f
timpt0261 <rchavez0215@gmail.com>
2024-02-14 23:53:08 -0800

    Modified UI Layout
    - Modified UI lay out to fix Aspect ratio
    - Added New animations from most recent Sprite Sheet
    -

--- Assets/Scripts/UI/OptionMenu.cs
+++ Assets/Scripts/UI/OptionMenu.cs
@@ -4,16 +4,25 @@ using UnityEngine.UI;
 
 public class OptionMenu : MonoBehaviour
 {
+
+     [Header("Option Menu")]
      [SerializeField] private GameObject options;
+
+     [Header("Player Components")]
+     [SerializeField] private GameObject player;
      [SerializeField] private PlayerRebindControls rebindControls;
+     [SerializeField] private GameObject cardUser;
 
      [SerializeField] private TextMeshPro pauseButtonText;
      private bool isPaused;
 
      private void Awake()
      {
+          player = GameObject.FindGameObjectWithTag("Player");
+          rebindControls = player.GetComponentInChildren<PlayerRebindControls>();
+          cardUser = GameObject.Find("CardUser");
+
           SetPauseState(false);
-          pauseButtonText.color = Color.clear;
      }
 
      public void OnPause()
@@ -25,7 +34,9 @@ public class OptionMenu : MonoBehaviour
      {
           isPaused = paused;
           options.SetActive(paused);
+          cardUser.SetActive(!paused);
           Time.timeScale = paused ? 0f : 1f;
+
      }
 
      public void OnRebindPlayerMovement()
