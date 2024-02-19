0cfe0bb6cf589b44788a4901fe35abead1041f21
DimirAgentAshley <52061722+DimirAgentAshley@users.noreply.github.com>
2024-02-19 15:53:24 -0800

    Scrolling UI for Deck Editor

--- Assets/Scripts/UI/GameplayUI/Deck Editing/DeckEditor.cs
+++ Assets/Scripts/UI/GameplayUI/Deck Editing/DeckEditor.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 using System.Collections.Generic;
 using NaughtyAttributes;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
-public enum CardPile { ownedCards, NULL, deck }
+public enum EditorCardPile { ownedCards, NULL, deck }
 
 public class DeckEditor : MonoBehaviour
 {
@@ -10,62 +12,74 @@ public class DeckEditor : MonoBehaviour
     public int minDeckSize = 10;
 
     // Debug contents.
-    [SerializeField]
-    private Card[] deck = new Card[] {};
-    private Card[] ownedCards = new Card[] {};
+    [SerializeField] private Transform scrollViewContent;
+    [SerializeField] private GameObject cardPrefab;
+    [SerializeField] public List<Card> ownedCards = null;
+    [SerializeField] public List<Card> deck = null;
 
-    private Dictionary<CardPile, List<Card>> pileToList = null;
+    private Dictionary<EditorCardPile, List<Card>> pileToList = null;
 
-    // Counts down the time until drawing a new card.
-    public float DrawTimer { get; private set; } = 0;
+    [Header("Global")]
+    [ReadOnly] public int w;
+    [ReadOnly] public int h;
+    public int cardFontSize = 50;
+
+    [Header("Cards")]
+    public int globalYOffset;
+    public int xWidth;
+    public Vector2 cardOffset = new();
+    public Vector2 cardDimensions = new(200,300);
+    private CardUser user;    
+
+    private GUIStyle cardStyle;
+    Texture2D normalBackground, hoverBackground;
+
+
 
     private void Awake()
     {
         // Awake is called before Start().
         // ================
-
         pileToList = new()
         {
-            {CardPile.NULL, null},
-            {CardPile.ownedCards, ownedCards},
-            {CardPile.drawPile, drawPile},
-            {CardPile.hand, hand},
-            {CardPile.discardPile, discardPile},
+            {EditorCardPile.NULL, null},
+            {EditorCardPile.ownedCards, ownedCards},
+            {EditorCardPile.deck, deck},
         };
+        // Create custom style for cards
+        normalBackground = new Texture2D(1, 1, TextureFormat.RGBAFloat, false); 
+        normalBackground.SetPixel(0, 0, new Color(0, 0, 0, 1f));
+        normalBackground.Apply();
+        // Create custom style for cards that are being hovered over
+        hoverBackground = new Texture2D(1, 1, TextureFormat.RGBAFloat, false); 
+        hoverBackground.SetPixel(0, 0, new Color(0.025f, 0.025f, 0.05f, 1f));
+        hoverBackground.Apply();
 
-        drawPile = new List<Card>(DEBUG_startingDeck);
-        Shuffle(drawPile);
-                  // Don't draw more cards than we have.
-        DrawCards(Mathf.Min(startingHandSize, drawPile.Count));
-    }
-
-    private void Update()
-    {
-        // Cards are drawn regularly over time. Update() detects when it's time
-        // to do so.
-        // ================
-
-        if (DrawTimer > drawDelay)
+        foreach (Card card in ownedCards)
         {
-            if (hand.Count < maxHandSize)
-            {
-                DrawCards(normalDrawAmount);
-                DrawTimer = 0;
-            }
-            else
-            {
-                DrawTimer = drawDelay;
-            }
+            Debug.Log("card");
+            GameObject cardRender = Instantiate(cardPrefab, scrollViewContent);
         }
 
-        DrawTimer += Time.deltaTime;
     }
 
     // ================================================================
     // Public interface methods
     // ================================================================
+    private void OnGUI()
+    {
+        w = Screen.width;
+        h = Screen.height;
+        
+        cardStyle = new GUIStyle(GUI.skin.box); 
+        cardStyle.normal.textColor = Color.white;
+        cardStyle.normal.background = normalBackground;
+        cardStyle.hover.textColor = Color.cyan;
+        cardStyle.hover.background = hoverBackground;
+        cardStyle.fontSize = cardFontSize;
+    }
 
-    public void ShuffleDiscardIntoDrawpile()
+    /*public void ShuffleDiscardIntoDrawpile()
     {
         // We shuffle the discard pile first, and then pop it to draw pile.
         // This way, if we're shuffling into a non-empty deck, only the new
@@ -75,30 +89,15 @@ public class DeckEditor : MonoBehaviour
 
         Shuffle(discardPile);
         PopFromPushTo(discardPile, drawPile, discardPile.Count);
-    }
+    }*/
 
-    public void DrawCards(int n)
-    {
-        // Tries to push n number of cards from the drawPile and pushes them 
-        // into our hand. Pushes as many cards as possible if it can't push n
-        // number of cards.
-        // ================
 
-        if (drawPile.Count < n)
-        {
-            ShuffleDiscardIntoDrawpile();
-        }
-        PopFromPushTo(drawPile, hand, Mathf.Min(n, drawPile.Count));
-
-        
-    }
-
-    public void Discard(Card card)
+    /*public void Discard(Card card)
     {
         RemoveFromPushTo(card, hand, discardPile);
-    }
-
-    public void MoveCard(Card card, CardPile fromPile, CardPile toPile)
+    }*/
+    
+    public void MoveCard(Card card, EditorCardPile fromPile, EditorCardPile toPile)
     {
         // Acts as a public accessor for the RemoveFromPushTo function.
         // Moves a card from fromPile to the start of toPile.
@@ -109,74 +108,16 @@ public class DeckEditor : MonoBehaviour
         RemoveFromPushTo(card, pileToList[fromPile], pileToList[toPile]);
     }
 
-    public Card GetRandom(CardPile pile)
+    public Card GetRandom(EditorCardPile pile)
     {
         List<Card> pileList = pileToList[pile];
         return pileList[Random.Range(0, pileList.Count)];
-    }
-
-    public void UseCard(Card card, Card.UseMode useMode, Targetable target, bool removeFromDeck=false)
-    {
-        // TARGETABLE target: Uses a card with the given useMode {Play, Throw}.
-        // ================
-
-        if (ValidateUse(card, useMode) == false) { return; }
-
-        if (!removeFromDeck) Discard(card);
-        card.Use(this, useMode, target);
-    }
-
-    public void UseCard(Card card, Card.UseMode useMode, Vector3 target, bool removeFromDeck=false)
-    {
-        // VECTOR3 target: Uses a card with the given useMode {Play, Throw}.
-        // ================
-
-        if (ValidateUse(card, useMode) == false) { return; }
-
-        card.Use(this, useMode, target);
-        if (!removeFromDeck) Discard(card);
-    }
-
-    public void UseCard(Card card, Card.UseMode useMode, bool removeFromDeck=false)
-    {
-        // TARGETLESS target: Uses a card with the given useMode {Play, Throw}.
-        // ================
-
-        if (ValidateUse(card, useMode) == false) { return; }
-
-        card.Use(this, useMode);
-        if (!removeFromDeck) Discard(card);
-    }
-
-    private bool ValidateUse(Card card, Card.UseMode useMode)
-    {
-        if (useMode == Card.UseMode.NULL)
-        {
-            Debug.LogError($"CardUser Error. UseCard failed. NULL is not a valid UseMode.", this);
-            return false;
-        }
-        if (!hand.Contains(card))
-        {
-            Debug.LogError($"CardUser Error. UseCard failed. Hand does not contain card {card}.", this);
-            return false;
-        }
-        return true;
     }
 
     // ================================================================
     // Pile-editing methods
     // ================================================================
 
-    private void Shuffle(List<Card> pile)
-    {
-        // Shuffles list pile in place using the Fisher-Yates algorithm.
-        // ================
-
-        for (int i = pile.Count-1; i > 0; i--)
-        {
-            SwapItems(pile, i, Random.Range(0,i));
-        }
-    }
 
     private void SwapItems<T>(List<T> pile, int indexA, int indexB)
     {
