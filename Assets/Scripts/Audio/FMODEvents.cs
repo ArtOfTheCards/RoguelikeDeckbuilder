7d2c5d444696cfe8002e40264c1b4afaeb794c87
emmapy <emmapyates@gmail.com>
2024-03-12 22:29:07 -0700

    card SFX: magic missile, skeleton summon, venemous bite

--- Assets/Scripts/Audio/FMODEvents.cs
+++ Assets/Scripts/Audio/FMODEvents.cs
@@ -5,14 +5,22 @@ using FMODUnity;
 
 public class FMODEvents : MonoBehaviour
 {
-    [field: Header("Draw SFX")]
-    [field: SerializeField] public EventReference DrawCard { get; private set; }
-
-    [field: Header("PlayerFootsteps SFX")]
+    [field: Header("PLAYER")]
     [field: SerializeField] public EventReference PlayerFootsteps { get; private set; }
     
-    [field: Header("Hover SFX")]
+
+    [field: Header("CARDS")]
+    [field: SerializeField] public EventReference DrawCard { get; private set; }
     [field: SerializeField] public EventReference CardOnHover { get; private set; }
+    //============
+    [field: SerializeField] public EventReference BurstAttack { get; private set; } // aka Magick Missile
+    [field: SerializeField] public EventReference SpawnSkeletons { get; private set; } // aka Skeleton Summon
+    [field: SerializeField] public EventReference Throw { get; private set; }
+    [field: SerializeField] public EventReference AddPoison { get; private set; } // aka Venemous Bite
+
+    [field: Header("ENEMIES")]
+ 
+    public string test = "dummy";
 
     public static FMODEvents instance { get; private set; }
 
