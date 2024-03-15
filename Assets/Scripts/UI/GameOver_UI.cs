41c372786b2e91ce880a2b847d2356927fd6e7c1
timpt0261 <rchavez0215@gmail.com>
2024-03-14 20:52:52 -0700

    Added Working GameOver
    - Modified Nav Mesh Agent
    - Modified OptionMenu.cs to find player components
    - Modified Working Game Over UI

--- Assets/Scripts/UI/GameOver_UI.cs
+++ Assets/Scripts/UI/GameOver_UI.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOver_UI : MonoBehaviour
 {
+    [SerializeField] private AsyncLoader loader;
     [SerializeField] private GameObject optionMenu;
-    [SerializeField] private Damagable player;
-    [SerializeField] private Damagable enemy;
+    [SerializeField] private GameObject gameOver;
+    [SerializeField] private GameObject player;
+
+    [SerializeField] private GameObject enemy;
+    [SerializeField] private Damagable player_dmg;
+    [SerializeField] private Damagable enemy_dmg;
     [SerializeField] private TextMeshProUGUI text;
 
     private string message; // Declare the message variable
@@ -14,7 +21,15 @@ public class GameOver_UI : MonoBehaviour
     private void Awake()
     {
         // Ensure the game over UI is initially inactive
-        gameObject.SetActive(false);
+        player = GameObject.FindGameObjectWithTag("Player");
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+
+        player_dmg = player.GetComponentInChildren<Damagable>();
+        enemy_dmg = enemy.GetComponentInChildren<Damagable>();
+
+        loader = GameObject.FindObjectOfType<AsyncLoader>();
+        gameOver.SetActive(false);
+
     }
 
     private void Update()
@@ -25,15 +40,17 @@ public class GameOver_UI : MonoBehaviour
 
     private void CheckDamagable()
     {
+        Debug.Log("Player's HealthBar: " + player_dmg.currentHealth + "\n");
+        Debug.Log("Enemies's HealthBar: " + enemy_dmg.currentHealth + "\n");
         // If player's health is zero, game over, player loses
-        if (player.currentHealth == 0)
+        if (player_dmg.currentHealth == 0)
         {
             message = "Game Over, You Lose";
             GameOver();
         }
 
         // If enemy's health is zero, game over, player wins
-        if (enemy.currentHealth == 0)
+        if (enemy_dmg.currentHealth == 0)
         {
             message = "You Win";
             GameOver();
@@ -52,6 +69,25 @@ public class GameOver_UI : MonoBehaviour
         Time.timeScale = 0;
 
         // Ensure the game over UI is visible
-        gameObject.SetActive(true);
+        gameOver.SetActive(true);
+
+        player.SetActive(false);
+        enemy.SetActive(false);
+    }
+
+
+    public void Restart()
+    {
+        loader.LoadlevelBtn("Arena");
+    }
+
+    public void Menu()
+    {
+        loader.LoadlevelBtn("TitleScreen");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
